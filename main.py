45ac74c9c5f419677e17f322c6002be7815b4743
opeRaptor <testsubjectnamehere@gmail.com>
2020-11-26 16:18:24 +0100

    ported away from uGFX -> display lib and added PNG top board with temperature~

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import ugfx, gc, wifi, badge, system, time, display, machine
-from time import sleep, localtime
+import gc, wifi, badge, time, display, machine
+from time import sleep
 import urequests as requests
 
 try:
@@ -12,33 +12,63 @@ try:
     departureDir= [None] * 4
     departureLine= [None] * 4
 
-    refreshCounter = 0
+    refreshCounter = 10
     updateDisplay = True
-    
+###############################################
+    BADGE_EINK_LUT_CUSTOM  = -1
+    BADGE_EINK_LUT_FULL    =  4
+    BADGE_EINK_LUT_NORMAL  =  8
+    BADGE_EINK_LUT_FASTER  =  16
+    BADGE_EINK_LUT_FASTEST =  32
+###############################################
+
     while True:
-        ugfx.init()
+        #ugfx.init()
         display.orientation(270)
+        
         try:
             wifi.init()
+            #wifi.connect()
             while not wifi.sta_if.isconnected():
                 sleep(0.1)
         except:
             wifi.connect()
+            print("Connecting to WiFi")
             if not wifi.wait():
                 time.sleep(2)
-                system.home()
+                print("Connection Error, Rebooting")
+                machine.reset()
 
 
         #############################################################################################################################################################################################
         url = "http://travelplanner.mobiliteit.lu/restproxy/departureBoard?accessId=cdt&id=A=1@O=Lintgen,%20Kräizung@X=6,125115@Y=49,720344@U=82@L=160702003@B=1@p=1594365298&format=json"
         trainURL = "http://travelplanner.mobiliteit.lu/restproxy/departureBoard?accessId=cdt&id=A=1@O=Lintgen,%20gare&format=json"
-
+        trustmeURL = "http://192.168.179.1:8186/trustme.lua?accept="
+        #r = requests.get("http://192.168.179.1:8186/trustme.lua?accept=")
+       ################################ Guest Wifi Portal ######################################### 
         while True:
-            r = requests.get(url)
-            print("HTTP request status Code:",r.status_code)
+            trust = requests.get(trustmeURL)
+            print("Trust me HTTP request status Code:",trust.status_code)
             #print(r.status_code)
             
-            if r.status_code == 200:
+            if trust.status_code == 200:
+                break
+            else:
+                # Hope it won't 500 a little later
+                print("Bad response")
+                time.sleep(5)
+
+        #print("Trustme:",r.text)
+        trust.close()
+        gc.collect()
+        #########################################################################
+       
+        while True:
+            api = requests.get(url)
+            print("API HTTP request status Code:",api.status_code)
+            #print(r.status_code)
+            
+            if api.status_code == 200:
                 break
             else:
                 # Hope it won't 500 a little later
@@ -48,8 +78,8 @@ try:
         #print("Data")
         #print(r.text)
 
-        jsonData = r.json()
-        r.close()
+        jsonData = api.json()
+        api.close()
         gc.collect()
 
         print("JSON loaded")
@@ -61,6 +91,35 @@ try:
 
         #############################################################################################################################################################################################
         
+        tempurl= 'http://wttr.in/lintgen?format="%t"'
+        while True:
+            tempAPI = requests.get(tempurl)
+            print("Temperature HTTP request status Code:",tempAPI.status_code)
+            #print(r.status_code)
+            
+            if tempAPI.status_code == 200:
+                break
+            else:
+                # Hope it won't 500 a little later
+                print("Bad response")
+                time.sleep(5)
+
+        print("Temp Data:",tempAPI.text)
+
+        temp = tempAPI.text[1:]
+        
+        tempAPI.close()
+        gc.collect()
+
+        #temp = "-8dcc"
+        if temp[0] == "+":
+            temp = temp[1:]
+        temp = temp[:-3]
+
+        #print(temp)
+        
+        #############################################################################################################################################################################################
+        
         #####################################FILL IN DATA###############################################
         if len(jsonData) >= 4: #checks that there is returned data other than the 3 entry in the JSON footer
             numOfDepartures = len(jsonData['Departure'])
@@ -70,7 +129,7 @@ try:
             
             for x in range(0, numOfDepartures):
                 print("|||||||||||||||||||||| ",x," ||||||||||||||||||||||")
-                print(len(jsonData['Departure'][x])," returned cat. if more than 13, it is a late bus") ###
+                print(len(jsonData['Departure'][x])," returned cat. if more than 13, it is a late bus and ",departureTime[x], "not equal to",departureRealtime[x]) ###
                 
                 if len(jsonData['Departure'][x]) >= 14:
                     print("Storing departure time")
@@ -103,7 +162,11 @@ try:
                 #print ('"direction":', departureDir)
                 #print ('"line":', departureLine)
                 
-                departureDir[x] = departureDir[x][:12] #max number of char. that can display
+                #departureDir[x] = departureDir[x][:12] #max number of char. that can display
+                while display.getTextWidth(departureDir[x],"Roboto_Black22") >= (128+ 22):
+                    departureDir[x] = departureDir[x][:-1]
+                    #print("Len of dir.: ",display.getTextWidth(departureDir[x],"Roboto_Black22"))
+                    #print("Direction:",departureDir[x])
                 
                 print ('"direction":', departureDir[x])
                 print ('"line":', departureLine[x])
@@ -124,43 +187,60 @@ try:
             if updateDisplay == True:     
                 print("~~Updating display~~")    
                 
-                lut = ugfx.LUT_FASTER
+                lut = BADGE_EINK_LUT_FASTER
                 
                 refreshCounter = refreshCounter + 1
                 if refreshCounter >= 10: #full deep refresh after 10 normal prints
                     refreshCounter = 0
                     print("Full screen clear")
-                    ugfx.clear(ugfx.WHITE)
-                    lut = ugfx.LUT_FULL
+                    #display.clear(0xFFFFFF)
+                    display.drawFill(0xFFFFFF)
+                    lut = BADGE_EINK_LUT_FULL
                 else:
-                    lut = ugfx.LUT_FASTER
+                    lut = BADGE_EINK_LUT_FASTER
                 
-                ugfx.string_box(0, -8, 128, 30, "BUS TIME", "Roboto_Black22", ugfx.BLACK, ugfx.justifyCenter) #printing title
-                ugfx.area(0, 33+(0)*65, 128, 55, ugfx.BLACK)
-                ugfx.flush(lut)
+                #ugfx.string_box(0, -8, 128, 30, "BUS TIME", "Roboto_Black22", ugfx.BLACK, ugfx.justifyCenter) #printing title
+                display.drawPng(0,0,"/lib/luxPublicTransport/busBoardV2.png")
+                
+                display.drawText(106-int(display.getTextWidth(temp,"Roboto_Regular18")/2), 10, temp, 0xFFFFFF, "Roboto_Regular18")
+
+                display.drawCircle(120, 12, 2,0,360,True,0xFFFFFF)
+                display.drawCircle(120, 12, 1,0,360,True,0x000000)
+
+                #ugfx.area(0, 45+(0)*65, 128, 55, ugfx.BLACK)
+                display.drawRect(0, 45+(0)*65, 128, 55,True,  0x000000)
+                display.flush(lut)
 
                 for x in range(0, numOfDepartures):
 
-                    ugfx.fill_rounded_box (86, 38+x*65, 37, 20, -2, ugfx.WHITE) #printing white box for the line
-                    ugfx.string_box(86, 36+x*65, 37, 20, departureLine[x], "Roboto_Regular18", ugfx.BLACK, ugfx.justifyCenter) #printing line in white box
+                    #ugfx.fill_rounded_box (86, 50+x*65, 37, 20, -2, ugfx.WHITE) #printing white box for the line
+                    display.drawRect(86, 50+x*65, 37, 20,True,  0xFFFFFF)
+                    #ugfx.string_box(86, 48+x*65, 37, 20, departureLine[x], "Roboto_Regular18", ugfx.BLACK, ugfx.justifyCenter) #printing line in white box
+                    display.drawText(86+2, 50+(x*65)+16-int(display.getTextWidth(departureLine[x],"Roboto_Regular18")/2), departureLine[x], 0x000000, "Roboto_Regular18")
                     #ugfx.string(86+2, 37+x*65, departureLine[x], "Roboto_Regular18", ugfx.BLACK)
 
                     if departureRealtime[x] == None: #if realtime (if bus is late) is empty print scheduled time
-                        ugfx.string(4, 35+x*65, departureTime[x], "Roboto_Black22", ugfx.WHITE)
+                        #ugfx.string(4, 47+x*65, departureTime[x], "Roboto_Black22", ugfx.WHITE)
+                        display.drawText(4, 47+x*65, departureTime[x], 0xFFFFFF, "Roboto_Black22")
                     else: #if realtime (if bus is late) is has data print late time
-                        ugfx.string(4, 35+x*65, departureRealtime[x], "Roboto_Black22", ugfx.WHITE)
+                        #ugfx.string(4, 47+x*65, departureRealtime[x], "Roboto_Black22", ugfx.WHITE)
+                        display.drawText(4, 47+x*65, departureRealtime[x], 0xFFFFFF, "Roboto_Black22")
                         #ugfx.fill_circle(x, y, r, colour)
-                        ugfx.fill_circle(76, 47+x*65, 5, ugfx.WHITE) #add dot to show the bus is late
+                        #ugfx.fill_circle(76, 57+x*65, 5, ugfx.WHITE) #add dot to show the bus is late
+                        display.drawCircle(76, 59+x*65, 3,0,360,True,0xffffff)
                     
-                    ugfx.string(4, 35+28+x*65, departureDir[x], "Roboto_Regular18", ugfx.WHITE) # print direction
+                    #ugfx.string(4, 45+28+x*65, departureDir[x], "Roboto_Regular18", ugfx.WHITE) # print direction
+                    display.drawText(4, 45+28+x*65, departureDir[x], 0xFFFFFF, "Roboto_Regular18")
                     
 
                     if x+1 < numOfDepartures:
-                        ugfx.area(0, 33+(x+1)*65, 128, 55, ugfx.BLACK)
+                        #ugfx.area(0, 45+(x+1)*65, 128, 55, ugfx.BLACK)
+                        display.drawRect(0, 45+(x+1)*65, 128, 55,True,  0x000000)
                     else:
-                        ugfx.area(0, 33+(x+1)*65, 128, 55, ugfx.WHITE)
+                        #ugfx.area(0, 45+(x+1)*65, 128, 55, ugfx.WHITE)
+                        display.drawRect(0, 45+(x+1)*65, 128, 55,True,  0xFFFFFF)
 
-                    ugfx.flush(ugfx.LUT_FASTER)
+                    display.flush(BADGE_EINK_LUT_FASTER)
             else:
                 print("No change in data, no screen update needed")
         #####################################No data from API###############################################
@@ -169,9 +249,16 @@ try:
             refreshCounter = refreshCounter + 1
 
             numOfDepartures=0
-            ugfx.clear(ugfx.WHITE)
-            ugfx.string_box(0, -8, 128, 30, "BUS TIME", "Roboto_Black22", ugfx.BLACK, ugfx.justifyCenter)
-            ugfx.flush()
+            #ugfx.clear(ugfx.WHITE)
+            #display.clear(0xFFFFFF)
+            display.drawFill(0xFFFFFF)
+            #ugfx.string_box(0, -8, 128, 30, "BUS TIME", "Roboto_Black22", ugfx.BLACK, ugfx.justifyCenter)
+            display.drawPng(0,0,"/lib/luxPublicTransport/busBoardV2.png")
+            
+            display.drawText(106-int(display.getTextWidth(temp,"Roboto_Regular18")/2), 10, temp, 0xFFFFFF, "Roboto_Regular18")
+            display.drawCircle(120, 12, 2,0,360,True,0xffffff)
+            display.drawCircle(120, 12, 1,0,360,True,0x000000)
+            display.flush()
 
         #############################################################################################################################################################################################
 
@@ -194,14 +281,14 @@ except:
     #system.reboot()
     machine.reset()
 
-# ################################################################################
+# # ################################################################################
 # #make app run on boot
 # import machine
 # machine.nvs_setstr("system", "default_app", "luxPublicTransport")
 
-# ################################################################################
-# #install app from store after pushing to Hatchery
-# #Hatchery link: https://badge.team/projects/luxpublictransport/
+# # ################################################################################
+# # #install app from store after pushing to Hatchery
+# # #Hatchery link: https://badge.team/projects/luxpublictransport/
 
 # import wifi, woezel,machine
 # wifi.connect()
@@ -209,5 +296,81 @@ except:
 # woezel.install("luxPublicTransport")
 # machine.reset()
 
-# import machine
-# machine.reset()
+
+
+
+# import urequests as requests
+# import wifi
+# wifi.connect()
+# wifi.wait()
+# while True:
+#     t = requests.get("http://192.168.179.1:8186/trustme.lua?accept=")
+#     print("Trust me HTTP request status Code:",t.status_code)
+#     #print(r.status_code)
+    
+#     if t.status_code == 200:
+#         break
+#     else:
+#         # Hope it won't 500 a little later
+#         print("Bad response")
+#         time.sleep(5)
+
+# #print("Trustme:",r.text)
+# t.close()
+
+
+
+
+
+# import display,wifi
+# import urequests as requests
+# display.orientation(270)
+
+
+# try:
+#     wifi.init()
+#     while not wifi.sta_if.isconnected():
+#         sleep(0.1)
+# except:
+#     wifi.connect()
+#     print("Connecting to WiFi")
+#     if not wifi.wait():
+#         time.sleep(2)
+#         print("Connection Error, Rebooting")
+#         machine.reset()
+
+# url= 'http://wttr.in/?format="%t"'
+# while True:
+#     r = requests.get(url)
+#     print("API HTTP request status Code:",r.status_code)
+#     #print(r.status_code)
+    
+#     if r.status_code == 200:
+#         break
+#     else:
+#         # Hope it won't 500 a little later
+#         print("Bad response")
+#         time.sleep(5)
+
+# print("Data:",r.text)
+
+# temp = r.text[1:]
+# #temp = "-8dcc"
+# if temp[0] == "+":
+#     temp = temp[1:]
+# temp = temp[:-3]
+
+# print(temp)
+# width = display.getTextWidth(temp,"Roboto_Regular18")
+# widthHalf = int(width/2)
+# print("text width:",width)
+
+# display.drawFill(0xffffff) # Fill the screen with black
+# display.drawPng(0,0,"/lib/luxPublicTransport/busBoardV2.png")
+# display.drawText(106-widthHalf, 10, temp, 0xFFFFFF, "Roboto_Regular18")
+# display.drawCircle(120, 12, 2,0,360,True,0xffffff)
+# display.drawCircle(120, 12, 1,0,360,True,0x000000)
+
+# for x in range (0,4):
+#     display.drawRect(0, 45+(x)*65, 128, 55,True,  0x000000)
+#     display.flush()
